a8d55e8e2550731334cd4b302b0da57fed6bfbb8
iCoder-007 <ssygi0007@gmail.com>
2021-01-17 09:52:26 -0500

    fixed issues

--- pixelendgraphics/settings.py
+++ pixelendgraphics/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = '*eg)8qy0mcsv=gex1s5b^2t43c6%7w%v#u=d7(q)h!b5fymtuf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
 
-ALLOWED_HOSTS=[]
-# ALLOWED_HOSTS = ['103.228.113.57' , 'pixelngraphics.com', 'www.pixelngraphics.com']
+# ALLOWED_HOSTS=[]
+ALLOWED_HOSTS = ['103.228.113.57' , 'pixelngraphics.com', 'www.pixelngraphics.com']
 # Application definition
 
 INSTALLED_APPS = [
@@ -85,22 +85,22 @@ WSGI_APPLICATION = 'pixelendgraphics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'pixelngraphics',
-#         'USER': 'pixel',
-#         'PASSWORD': 'pixel@n',
-#         'HOST': 'localhost',
-            
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pixelngraphics',
+        'USER': 'pixel',
+        'PASSWORD': 'pixel@n',
+        'HOST': 'localhost',
+            
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
@@ -142,10 +142,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-   os.path.join(BASE_DIR,'static'),    
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS=[
+#    os.path.join(BASE_DIR,'static'),    
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
